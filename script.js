56313fbc4c4f7b1ffef16deadaf43fd99ee33da6
Charles042 <ekechukwuma20@gmail.com>
2024-01-28 13:13:46 +0100

    rent movie app

---
+++ script.js
@@ -0,0 +1,14 @@
+//Creating a class
+
+class Ginger {
+// static property
+
+static bestTalent = 'Known to have a beautiful voice';
+
+constructor(talent1, talent2, talent3){
+    this.good = talent1;
+    this.bad = talent2;
+    this.terrible = talent3;
+}
+
+}
